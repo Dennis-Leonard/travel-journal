609884879712e070a7cca338d2a1de83bae6be7c
Dennis-Leonard <dennis.leonard@gmail.com>
2025-08-07 10:56:29 -0400

    initial code push

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import Globe from '../images/globe.png';
+const Header = () => {
+  return (
+    <header>
+      <img src={Globe} alt="globe icon" />
+      <h1>my travel journal.</h1>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// https://www.youtube.com/watch?v=x4rFhThSX04
+// 3:47:48 review array map
+
